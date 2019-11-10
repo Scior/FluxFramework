be277bd7b811ed2594ed612c028686b80208b1b0
Scior <interval12@gmail.com>
2019-11-10 15:02:09 +0900

    Initial commit

---
+++ FluxFramework/FluxFramework.h
@@ -0,0 +1,19 @@
+//
+//  FluxFramework.h
+//  FluxFramework
+//
+//  Created by Suita Fujino on 2019/11/10.
+//  Copyright © 2019 Suita Fujino. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FluxFramework.
+FOUNDATION_EXPORT double FluxFrameworkVersionNumber;
+
+//! Project version string for FluxFramework.
+FOUNDATION_EXPORT const unsigned char FluxFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluxFramework/PublicHeader.h>
+
+
